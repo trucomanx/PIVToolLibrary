616632b83c28902a5fc14d07413f29f9a29ec100
TrucomanX <fernando.pujaico.rivera@gmail.com>
2015-10-25 19:15:03 -0200

    Terminado per haciendo tests

--- teste.java
+++ teste.java
@@ -1,26 +1,45 @@
-//javac  -classpath . teste.java
-
-import net.trucomanx.pdsplibj.pdspiv.PdsPoints;
+//javac  -classpath .:pdsplibj.jar teste.java
+//java  -classpath .:pdsplibj.jar teste
+import net.trucomanx.pdsplibj.pdspiv.*;
 import java.awt.Point;
 
 public class teste {
 	
 	public static void main (String args[]) {
-		int L=8;
-		
-		PdsPoints pts =new PdsPoints(L);
-		PdsPoints pts2=new PdsPoints(L);
-		Point p;
-		
-		for(int i=0;i<L;i++)
-		{
-			p=pts.get_point_from_id(i);
-			System.out.println("Point:"+p);
-			pts.set_point(i,i-1,i+1);
-			System.out.println("Point:"+pts.get_point_from_id(i));
-		}
-		pts2.set_points(pts);
-		System.out.println("Point:\n"+pts2);
+		// Configuração de dados do match %%%
+		PdsPivConf conf=new PdsPivConf();
+		PdsPiv     piv =new PdsPiv();
+		PdsPivData data;
+		PdsPoints points1;
+		PdsPoints points2;
+		PdsPoints vec1to2;
+
+		double[] lins;
+		double[] dlins;
+		double[] cols;
+		double[] dcols;
+
+		conf.set_roi_window_size(64);
+		conf.set_search_step_size(8);			
+		conf.set_search_max_length(128);		
+		conf.set_points_by_columns(10);			
+		conf.set_points_by_lines(10);
+		conf.set_verbose_flag(1);			
+
+		// Trabalhando com PIV %%%
+		piv.load("images/img1.bmp","images/img2.bmp");
+	
+		data=piv.match(conf);
+
+		points1 = data.get_initial_points();
+		points2 = data.get_final_points();
+		vec1to2 = data.get_vectors();
+
+		lins=points1.get_array_from_value0();
+		cols=points1.get_array_from_value1();
+
+		dlins=vec1to2.get_array_from_value0();
+		dcols=vec1to2.get_array_from_value1();
 		
 	}
 }
--- teste_match.m
+++ teste_match.m
@@ -3,31 +3,50 @@ javaaddpath ("pdsplibj.jar");
 
 conf   =javaObject("net.trucomanx.pdsplibj.pdspiv.PdsPivConf");
 piv    =javaObject("net.trucomanx.pdsplibj.pdspiv.PdsPiv");
-data   =javaObject("net.trucomanx.pdsplibj.pdspiv.PdsPivData",10);
-
 
 %%% Configuração de dados do match %%%
-conf.set_roi_window_size(64);			%%[OK]
-conf.set_search_step_size(8);			%%[OK]
-conf.set_search_max_length(128);		%%[OK]
-
+conf.set_roi_window_size(50);			%%[OK]
+conf.set_search_step_size(2);			%%[OK]
+conf.set_search_max_length(100);		%%[OK]
+conf.set_points_by_columns(8);			%%[OK]
+conf.set_points_by_lines(6);			%%[OK]
+conf.set_verbose_flag(1);
 
 %%% Trabalhando com PIV %%%
-%piv.load(img1,img2);
-b=piv.load("images/img1.bmp","images/img2.bmp")
+piv.load("images/img1.bmp","images/img2.bmp");
 	
-%data=piv.match(conf);
+data=piv.match(conf);
 
 %%% separando os resultados %%%
 points1 = data.get_initial_points();	%%[OK]
 points2 = data.get_final_points();		%%[OK]
 vec1to2 = data.get_vectors();			%%[OK]
 
-%%% Usando os resultados %%%
-N =points1.get_length();				%%[OK]
+im2  = imread("images/img2.bmp");
+figure(2);
+imshow(im2);
 
-p1=points1.get_point_from_id(0);		%%[OK]
-p2=points1.get_point_from_id(1);		%%[OK]
-% ...
-pN=points1.get_point_from_id(N-1);		%%[OK]
+lins =points1.get_array_from_value0();	cols =points1.get_array_from_value1();
+lins2=points2.get_array_from_value0();	cols2=points2.get_array_from_value1();
+dlins=vec1to2.get_array_from_value0();	dcols=vec1to2.get_array_from_value1();
+
+valid_ids=find(isnan(dlins)==0);
+
+lins = lins(valid_ids);		cols = cols(valid_ids);
+lins2= lins2(valid_ids);	cols2= cols2(valid_ids);
+dlins= dlins(valid_ids);	dcols= dcols(valid_ids);
+
+
+H=piv.get_image_height();
+W=piv.get_image_width();
+[lins';cols']
+[lins2';cols2']
+
+hold on;
+%axis([0 W 0 H]); axis([0 W-1 0 H-1]); quiver(cols,lins,dcols,dlins);
+axis([0 W 0 H]); scatter(cols,lins,"g");
+axis([0 W 0 H]); scatter(cols2,lins2,"r");
+%axis([0 W 0 H]); quiver(cols(1:4),lins(1:4),dcols(1:4),dlins(1:4),"y");
+hold off;
+
 
