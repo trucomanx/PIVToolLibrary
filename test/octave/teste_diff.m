e87bc7a94a06b9b4b301055d3624ca4e6a80f259
TrucomanX <fernando.pujaico.rivera@gmail.com>
2016-05-08 06:45:01 -0300

    fixing error

--- test/octave/teste_diff.m
+++ test/octave/teste_diff.m
@@ -1,10 +1,15 @@
-%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Preambulo
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+close all
 clear
 addpath('mfiles')
 
 debug_java (1)
 % Carregando java
 javaaddpath ("../../lib/pdsplibj.jar");
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %java_matrix_autoconversion (true);
 
@@ -15,14 +20,14 @@ conf   =javaObject("net.trucomanx.pdsplibj.pdspiv.PdsPivConf");
 piv    =javaObject("net.trucomanx.pdsplibj.pdspiv.PdsPiv");
 
 
-%%% Configuração de dados do match %%%
+%%% Configuração de dados do match 
 [NUM_OF_FIRST NUM_OF_LAST FATOR_MM_PX]=configure_piv_values(conf);
 
+%%% Configuração dos pontos iniciais
 FILENAME_OF_FIRST_IMAGE = [DIRECTORY_OF_IMAGES,filesep,num2str(NUM_OF_FIRST),'.bmp'];
-
 [Pin Nl Nc]=select_initial_grid_points(FILENAME_OF_FIRST_IMAGE,conf);
 
-KK=1:
+KK=1;
 Pout{KK}=Pin;
 plot_rectangle_in_image(FILENAME_OF_FIRST_IMAGE,Pout{1},conf);
 print([DIRECTORY_OF_RESULTS,filesep,num2str(1),'.png'],'-dpng');
@@ -34,12 +39,13 @@ for II=(NUM_OF_FIRST+STEP):NUM_OF_LAST
 	FILENAME1= [DIRECTORY_OF_IMAGES,filesep,num2str(II-STEP),'.bmp'];
 	FILENAME2= [DIRECTORY_OF_IMAGES,filesep,num2str(II  ),'.bmp'];
 
-	piv.load(	FILENAME1,FILENAME2);
+	piv.load(FILENAME1,FILENAME2);
 
 	disp(['calculando tracking:',num2str(II-STEP),'-',num2str(II)]);
 	KK=KK+1;
 	Pout{KK}=piv.tracking_points(Pin,conf);
 
+	close all
 	plot_rectangle_in_image(FILENAME2,Pout{KK},conf);
 	print([DIRECTORY_OF_RESULTS,filesep,num2str(II),'.png'],'-dpng');
 	%Pout{JJ}.toString()
@@ -53,6 +59,11 @@ print([DIRECTORY_OF_RESULTS,filesep,'endroute.png'],'-dpng');
 
 save_data_in_directory(DIRECTORY_OF_RESULTS,Pout);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Limpieza
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 javarmpath ("../../lib/pdsplibj.jar");
 rmpath('mfiles')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
