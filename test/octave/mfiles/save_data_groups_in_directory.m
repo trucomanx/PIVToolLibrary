d8ca5fde61378b4a3caed7cac315476bb4588212
trucomanx <fernando.pujaico.rivera@gmail.com>
2016-09-15 15:11:50 -0300

    Ordenando documentacion

---
+++ test/octave/mfiles/save_data_groups_in_directory.m
@@ -0,0 +1,70 @@
+function save_data_groups_in_directory(DIRECTORY,Nc,Nl,FATOR_MM_PX,Pout)
+	%% Nc : Numero de sectores analizados.
+	%% Nl : Numero de copias por sectores.
+
+	N=length(Pout);					%% Numero d iteraciones
+	Npoints=Pout{1}.get_length();	%% Numero de puntos en las iteraciones = Nc * Nl
+
+
+	for KK=0:(Nc-1)
+
+		lin=zeros(1,N);
+		col=zeros(1,N);
+		lin0=0;
+		col0=0;
+		for II=0:(Nl-1)
+			[lint colt lint0 colt0]=normalize_in_relation_to_first(Pout,KK*Nl+II);
+			lin=lin+lint;
+			col=col+colt;
+			lin0=lin0+lint0;
+			col0=col0+colt0;
+		end
+		lin=lin/Nl;
+		col=col/Nl;
+		lin0=lin0/Nl;
+		col0=col0/Nl;
+
+		print_and_save(DIRECTORY,KK,'lin',lin,FATOR_MM_PX);
+		print_and_save(DIRECTORY,KK,'col',col,FATOR_MM_PX);
+	end
+
+end
+
+%% retona a linha e columna no tempo, do KK essimo ponto, 
+%% referenciado ao primeiro de modo que o primeiro valor de lin e col é zeros
+%% a função retorna o verdadero primeiro valor de lin e col
+function [lin col lin0 col0]=normalize_in_relation_to_first(Pout,KK)
+	% Ponto K-esimo no tempo 0
+	lin0=Pout{1}.get_point_from_id(KK).getX();
+	col0=Pout{1}.get_point_from_id(KK).getY();
+
+	N=length(Pout);					%% Numero de iteraciones
+
+	lin=zeros(1,N);
+	col=zeros(1,N);
+
+	for II=1:N
+		if size(Pout{II}.get_point_from_id(KK),1)==1
+			lin(II)=Pout{II}.get_point_from_id(KK).getX()-lin0;
+			col(II)=Pout{II}.get_point_from_id(KK).getY()-col0;
+		else
+			lin(II)=NaN;
+			col(II)=NaN;
+		end
+	end
+end
+
+
+%%
+%%
+function print_and_save(DIRECTORY,KK,TEXT,VAR,FATOR_MM_PX)
+		FILEDATCOL=[DIRECTORY,filesep,'Point',num2str(KK),'-',TEXT,'.dat'];
+		FILEPNGCOL=[DIRECTORY,filesep,'Point',num2str(KK),'-',TEXT,'.png'];
+		N=length(VAR);
+		VARIABLE=VAR*FATOR_MM_PX;
+		plot([0:(N-1)],VARIABLE,'-s');
+		xlabel('Sample time');
+		ylabel('Dislocation');
+		print(FILEPNGCOL,'-dpng');
+		save( FILEDATCOL,'VARIABLE','-ascii');
+end
--- test/octave/mfiles/select_initial_grid_points.m
+++ test/octave/mfiles/select_initial_grid_points.m
@@ -1,14 +1,25 @@
-function [Pin Nl Nc]=select_initial_grid_points(filename,conf)
+function [Pin Nl Nc]=select_initial_grid_points(filename,conf,varargin)
+
+	DATA=[3 5 10];
+
+	if(nargin>=3)
+		DirWithData=varargin{1};
+		FileWithData=varargin{2};
+		D=load([DirWithData,filesep,FileWithData]);
+		DATA(1)=D.number_of_points_by_line;
+		DATA(2)=D.number_of_points_by_column;
+		DATA(3)=D.horizontal_displacement_in_pixels;
+	end
 
 	WSIZE=conf.get_roi_window_size();	
 
-	prompt1 = {'Number of points by line:'	,'Number of points by column:'	,'vertical displacement in pixels:'};
-	defin1  = {'5'							,'1'							,'30'};
-
+	prompt1 = {'Number of points by line:'	,'Number of points by column:'	,'Horizontal displacement in pixels:'};
+	defin1  = {num2str(DATA(1))				,num2str(DATA(2))				,num2str(DATA(3))};
 	dlg_title = 'Select points';
 	num_lines = 1;
 	
 	answer = inputdlg(prompt1,dlg_title,num_lines,defin1);
+
 
 	Nc=str2num(answer{1});
 	Nl=str2num(answer{2});
@@ -19,29 +30,32 @@ function [Pin Nl Nc]=select_initial_grid_points(filename,conf)
 	col=zeros(1,N);
 	lin=zeros(1,N);
 
+
+
 	img  = imread(filename);
-	hf=figure;	imshow(img);
-	[ac,al]=ginput(1);
-	ac=round(ac);
-	al=round(al);
-	NCOL=size(img,2);
-	deltac=(NCOL-2*(ac-1))/Nc;
-	deltal=str2num(answer{3});
+	hf=figure;	
+	imshow(img);
 
-	for c=0:(Nc-1)
-	for l=0:(Nl-1)
-		col(c*Nl+l+1)=ac+c*deltac;
-		lin(c*Nl+l+1)=al+l*deltal;
-	end
-	end
+	for II=1:Nc
+		hold on
 
+		[ac,al]=ginput(1);
+		ac=round(ac);	al=round(al);
 
-	hold on
-	for II=1:N
-		scatter(col(II) ,lin(II) ,"r");
-		rectangle('Position',[col(II), lin(II), WSIZE, WSIZE], 'LineWidth',3, 'EdgeColor','b');
+		L=str2num(answer{3});
+
+		for JJ=1:Nl
+			col((II-1)*Nl+JJ)=ac+round(L*randn(1));
+			lin((II-1)*Nl+JJ)=al;%+round(L*randn(1));
+		end
+
+		for KK=((II-1)*Nl+[1:Nl])
+			scatter(col(KK) ,lin(KK) ,"r");
+			rectangle('Position',[col(KK), lin(KK), WSIZE, WSIZE], 'LineWidth',3, 'EdgeColor','b');
+		end
+		hold off
+
 	end
-	hold off
 
 	msgbox ("Points selected");
 
